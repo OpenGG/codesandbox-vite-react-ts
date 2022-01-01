261ee044a58546d4274b3c6bf4984b7a3bdb27ae
Github CI <ci@github.com>
2022-01-01 10:44:07 +0000

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { hmr: { port: 443 } },
+  server: process.cwd() === "/sandbox" && {
+    hmr: {
+      port: 443
+    }
+  },
   plugins: [react()]
 })
